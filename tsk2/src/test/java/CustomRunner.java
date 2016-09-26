bf35f02bf84d8f0b8a29f8109724746b630152f3
Vlad.Litoshik <magistr-dante@yandex.ru>
2016-09-26 22:57:35 +0300

    change custom runner

--- tsk2/src/test/java/CustomRunner.java
+++ tsk2/src/test/java/CustomRunner.java
@@ -9,8 +9,8 @@ public class CustomRunner {
     public static void main(String[] args) {
         TestNG testNG = new TestNG();
         List<String> files = Arrays.asList(
-                ".TAT2/tsk2/src/test/resources/suites/BoyTestSuite.xml",
-                "./test/resources/suites/GirlTestSuite.xml");
+                "./tsk2/src/test/resources/suites/BoyTestSuite.xml",
+                "./tsk2/src/test/resources/suites/GirlTestSuite.xml");
         testNG.setTestSuites(files);
         testNG.run();
     }
