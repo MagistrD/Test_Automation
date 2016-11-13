7327e6707baeadc303ed00c28b69deb99063d535
Vlad.Litoshik <magistr-dante@yandex.ru>
2016-11-13 22:26:40 +0300

    Fix Aliexpress Cart Page - "Empty cart locator"

--- tsk8/src/main/java/aliexpress/page/CartPage.java
+++ tsk8/src/main/java/aliexpress/page/CartPage.java
@@ -6,7 +6,7 @@ public class CartPage extends Browser {
 
     private static final String DELETE = "Удалить";
     private static final String DELETE_BUTTON_LOCATOR = "//a[@class='remove-single-product']";
-    private static final String EMPTY_CART_TITLE = "Ваша корзина пока пуста";
+    private static final String EMPTY_CART_TITLE = "Ваша Корзина пуста";
 
     public boolean isProductPresent() {
         return isElementPresent(DELETE);
